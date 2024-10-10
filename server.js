9328730c54d80d9b90b360ec9ee0827084f0c883
suren67 <surendranpandi02@gmail.com>
2024-10-10 13:06:52 +0530

    test_10.10.2024_13.06

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ const fetchHumanizedText = async (inputText) => {
     modifiedText += ` ${randomVariation}`;
 
     const requestData = {
-        model: 'gpt-3.5-turbo',
+        model: 'gpt-4o',
         messages: [
             { 
                 role: 'user', 
